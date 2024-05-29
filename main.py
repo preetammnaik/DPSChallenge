f1c6cfc654d58ff73681dcfd305151158737ef01
Preetam <preetamn0@gmail.com>
2024-05-29 15:45:11 +0200

    DEV03: Created function to plot the bar graph of the data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 
 def readFileAndFilter(path):
@@ -13,7 +14,33 @@ def readFileAndFilter(path):
 
     return df
 
+def plotHistorical(df):
+    grouped_data = df.groupby(['Category', 'Year']).sum()['Value'].unstack().reset_index()
+    print(grouped_data)
+
+    melted_data = pd.melt(grouped_data, id_vars=['Category'], var_name='Year', value_name='Value')
+
+    print(melted_data)
+
+    # Plotting
+    fig = px.bar(melted_data, x='Year', y='Value', color='Category',
+                 title='Historical Number of Accidents per Category',
+                 labels={'Value': 'Number of Accidents', 'Year': 'Year'},
+                 hover_data={'Value': ':,.0f'})
+
+    fig.update_layout(
+        barmode='group',
+        xaxis_title='Year',
+        yaxis_title='Number of Accidents',
+        legend_title='Category',
+        hovermode='x unified',
+        template='plotly_white'
+    )
+
+    fig.show()
+
 
 data = readFileAndFilter('AccidentData.csv')
 
-print(data)
+
+plotHistorical(data)
