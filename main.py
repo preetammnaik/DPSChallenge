77b4a7376583efdd924325b4cb7d9b914f3381e9
Preetam <preetamn0@gmail.com>
2024-05-30 16:51:25 +0200

    DEV08: added Prophet model and training it and using it to make predictions

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import plotly.express as px
-from fbprophet import Prophet
+from prophet import Prophet
+import joblib
 
 
 def readFileAndFilter(path):
     df = pd.read_csv(path)
-
     df = df.rename(columns={'MONATSZAHL': 'Category', 'AUSPRAEGUNG': 'Accident-type',
                             'JAHR': 'Year', 'MONAT': 'Month', 'WERT': 'Value'})
-
-    df['MonthPart'] = df['Month'].apply(lambda x: None if x == 'Summe' else x[-2:])
-
-    df = df[df['MonthPart'].notna()]
-
+    df = df[df["Month"] != "Summe"]
     df = df[df['Year'] < 2021]
+    df = df[(df['Category'] == 'Alkoholunfälle') & (df['Accident-type'] == 'insgesamt')]
 
+    df['Month'] = df['Month'].astype(int) % 100
+    df['Date'] = pd.to_datetime(df['Year'].astype(str) + '-' + df['Month'].astype(str) + '-01')
     return df
-
 
 def plotHistoricalGraph(df):
     grouped_data = df.groupby(['Category', 'Year']).sum()['Value'].unstack().reset_index()
@@ -44,3 +42,34 @@ def plotHistoricalGraph(df):
 
     fig.show()
 
+def prepare_data_for_prophet(df):
+    df_prophet = df.groupby('Date').sum()['Value'].reset_index()
+    print(df_prophet['Date'])
+    df_prophet.columns = ['ds', 'y']
+    return df_prophet
+
+
+def trainProphetModel(data):
+    df_prophet = prepare_data_for_prophet(data)
+    model = Prophet()
+    model.fit(df_prophet)
+    joblib.dump(model, 'prophet_model.pkl')
+    return model
+
+
+def predict_accidents_prophet(year, month):
+    model = joblib.load('prophet_model.pkl')
+    date = pd.to_datetime(f'{year}-{month:02d}-01')
+    future = pd.DataFrame({'ds': [date]})
+    forecast = model.predict(future)
+    return int(forecast['yhat'].iloc[0])
+
+
+data = readFileAndFilter('AccidentData.csv')
+
+model = trainProphetModel(data)
+
+year = 2020
+month = 3
+predicted_value = predict_accidents_prophet(year, month)
+print(f'Predicted number of accidents for {year}-{month}: {predicted_value}')
